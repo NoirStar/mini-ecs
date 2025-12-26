5fa1785ac507799dd77a975e47ce078634a9ee4e
knw <sky_9233@naver.com>
2025-12-26 11:18:00 +0900

    feat: implement ComponentStorage with unordered_map

---
+++ component_storage.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <unordered_map>
+#include "entity.hpp"
+
+template<typename T>
+class ComponentStorage {
+public:
+    void add(Entity e, T component) {
+        data_.emplace(e, std::move(component));
+    }
+    
+    void remove(Entity e) {
+        data_.erase(e);
+    }
+    
+    T* get(Entity e) {
+        auto it = data_.find(e);
+        if (it != data_.end()) {
+            return &it->second;
+        }
+        return nullptr;
+    }
+
+    bool has(Entity e) const {
+        return data_.contains(e);
+    }
+
+private:
+    std::unordered_map<Entity, T> data_;
+};
